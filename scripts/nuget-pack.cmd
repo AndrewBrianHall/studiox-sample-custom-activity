c3ed12cbc2598384ceea939ba14be8c374c5a456
Andrew Hall <andrew.hall@uipath.com>
2021-01-07 11:18:12 -0500

    comments to pack script

--- scripts/nuget-pack.cmd
+++ scripts/nuget-pack.cmd
@@ -1,4 +1,7 @@
 
+REM NOTE: This script assumes that both nuget.exe is installed and on the %PATH%
+REM       and that is is run from Visual Studo's Developer Command Prompt
+
 SET _configuration=Debug
 IF /I "%1" == "-release" SET _configuration=Release
 
